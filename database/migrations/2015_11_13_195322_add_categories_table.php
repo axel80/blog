60f0cbd95c4a9d42e61d143e26c4ae5a661e1be6
Axel <creativo.hosannaweb@gmail.com>
2015-11-13 20:26:17 +0000

    Insertada migracion categorias video 04 curso Laravel 5.0

--- database/migrations/2015_11_13_195322_add_categories_table.php
+++ database/migrations/2015_11_13_195322_add_categories_table.php
@@ -3,7 +3,7 @@
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateUsersTable extends Migration {
+class AddCategoriesTable extends Migration {
 
 	/**
 	 * Run the migrations.
@@ -12,18 +12,14 @@ class CreateUsersTable extends Migration {
 	 */
 	public function up()
 	{
-		Schema::create('users', function(Blueprint $table)
+		Schema::create('categories', function(Blueprint $table)
 		{
 			$table->increments('id');
-			$table->string('name');
-			$table->string('email')->unique();
-			$table->string('password', 60);
-			$table->enum('type', ['member','admin'])->default('member');
-			$table->rememberToken();
+			$table->string('name', 60);
 			$table->timestamps();
 		});
 	}
-	
+
 	/**
 	 * Reverse the migrations.
 	 *
@@ -31,7 +27,7 @@ class CreateUsersTable extends Migration {
 	 */
 	public function down()
 	{
-		Schema::drop('users');
+		Schema::drop('categories');
 	}
 
 }
